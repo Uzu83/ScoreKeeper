6e368e6c754c40c4fa4cd995098bd0ed0dc1513c
Toshiki Togo <108942850+Uzu83@users.noreply.github.com>
2024-11-08 12:40:46 +0900

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by 東郷翔士希 on 2024/11/08.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
